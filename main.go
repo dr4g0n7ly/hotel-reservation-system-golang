8c3e5934ea5e4985fef1f9186f0e6a25282b0e6b
dr4g0n7ly <nishanth.pilli@gmail.com>
2024-04-13 15:26:37 +0530

    Set-up mongoDB

--- main.go
+++ main.go
@@ -1,11 +1,42 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
+
 	api "github.com/dr4g0n7ly/hotel-management-system-golang/api/handler"
+	"github.com/dr4g0n7ly/hotel-management-system-golang/types"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+const dburi = "mongodb://localhost:27017"
+const dbname = "hotel-reservation"
+const userColl = "users"
+
 func main() {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx := context.Background()
+	coll := client.Database(dbname).Collection(userColl)
+	user := types.User{
+		FirstName: "James",
+		LastName:  "Bond",
+	}
+	res, err := coll.InsertOne(ctx, user)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(res)
+
+	fmt.Println("Connected to MongoDB")
+	fmt.Println(client)
+
 	app := fiber.New()
 	apiv1 := app.Group("/api/v1")
 
