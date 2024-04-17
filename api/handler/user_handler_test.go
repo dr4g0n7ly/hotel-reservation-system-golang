8019d962093222a74d01e7ea5ba8a67c43681615
dr4g0n7ly <nishanth.pilli@gmail.com>
2024-04-17 22:51:18 +0530

    Update user_handler_test.go

--- api/handler/user_handler_test.go
+++ api/handler/user_handler_test.go
@@ -74,3 +74,43 @@ func TestPostUser(t *testing.T) {
 		t.Errorf("expected email %s but got %s", params.Email, user.Email)
 	}
 }
+
+func TestGetUser(t *testing.T) {
+	tdb := setup(t)
+	defer tdb.teardown(t)
+
+	app := fiber.New()
+	userHandler := NewUserHandler(tdb)
+
+	app.Post("/", userHandler.HandlePostUser)
+	params := types.CreateUserParams{
+		Email:     "foo@bar.com",
+		FirstName: "Foo",
+		LastName:  "Bar",
+		Password:  "password",
+	}
+	b, _ := json.Marshal(params)
+	req := httptest.NewRequest("POST", "/", bytes.NewReader(b))
+	req.Header.Add("Content-Type", "application/json")
+	res, _ := app.Test(req)
+	var postuser types.User
+	json.NewDecoder(res.Body).Decode(&postuser)
+	fmt.Println("USER ID: ", postuser.ID.Hex())
+
+	app.Get("/:id", userHandler.HandleGetUser)
+	req = httptest.NewRequest("GET", "/"+postuser.ID.Hex(), nil)
+	req.Header.Add("Content-Type", "application/json")
+	res, _ = app.Test(req)
+
+	var user types.User
+	json.NewDecoder(res.Body).Decode(&user)
+	if user.FirstName != params.FirstName {
+		t.Errorf("expected firstname %s but got %s", params.FirstName, user.FirstName)
+	}
+	if user.LastName != params.LastName {
+		t.Errorf("expected lastname %s but got %s", params.LastName, user.LastName)
+	}
+	if user.Email != params.Email {
+		t.Errorf("expected email %s but got %s", params.Email, user.Email)
+	}
+}
