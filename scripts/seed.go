73629a2b2f8d3765ead36932eedd241868c9bec6
dr4g0n7ly <nishanth.pilli@gmail.com>
2024-04-19 01:42:12 +0530

    Update seed.go

--- scripts/seed.go
+++ scripts/seed.go
@@ -21,20 +21,46 @@ func main() {
 	fmt.Println("Connected to MongoDB")
 
 	hotelStore := db.NewMongoHotelStore(client, db.DBNAME)
+	roomStore := db.NewMongoRoomStore(client, db.DBNAME)
+
 	hotel := types.Hotel{
 		Name:     "Golden Hotel",
 		Location: "Bucharest, Romania",
 	}
-	room := types.Room{
-		Type:  types.Single,
-		Price: 150,
-	}
-	_ = room
 
 	insertedHotel, err := hotelStore.InsertHotel(ctx, &hotel)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	rooms := []types.Room{
+		{
+			Type:  types.Single,
+			Price: 150.50,
+		},
+		{
+			Type:  types.Single,
+			Price: 150.50,
+		},
+		{
+			Type:  types.Deluxe,
+			Price: 200,
+		},
+		{
+			Type:  types.Suite,
+			Price: 1399,
+		},
+	}
+
+	for _, room := range rooms {
+		room.HotelID = insertedHotel.ID
+		insertedRoom, err := roomStore.InsertRoom(ctx, &room)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(insertedRoom)
+	}
+
 	fmt.Println("seeding the database")
 	fmt.Println(insertedHotel)
 }
