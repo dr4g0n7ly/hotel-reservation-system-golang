f1706632720bcc859962989836a15b225204af07
dr4g0n7ly <nishanth.pilli@gmail.com>
2024-04-20 01:34:03 +0530

    Create hotel_handler.go

---
+++ api/handler/hotel_handler.go
@@ -0,0 +1,22 @@
+package api
+
+import (
+	"github.com/dr4g0n7ly/hotel-management-system-golang/db"
+	"github.com/gofiber/fiber/v2"
+)
+
+type HotelHandler struct {
+	hotelStore db.HotelStore
+	roomStore  db.RoomStore
+}
+
+func newHotelHandler(h db.HotelStore, r db.RoomStore) *HotelHandler {
+	return &HotelHandler{
+		hotelStore: h,
+		roomStore:  r,
+	}
+}
+
+func (h *HotelHandler) HandleGetHotels(c *fiber.Ctx) error {
+	return nil
+}
