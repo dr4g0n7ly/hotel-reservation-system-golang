a9807899bd083d84cc564b899a9fa3235d124d22
dr4g0n7ly <nishanth.pilli@gmail.com>
2024-04-23 00:15:21 +0530

    Add jwtAuth middleware func

--- api/middleware/jwt.go
+++ api/middleware/jwt.go
@@ -2,12 +2,45 @@ package middleware
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 func JWTAuthentication(c *fiber.Ctx) error {
 	fmt.Println("-- JWT Auth")
 
+	token, ok := c.GetReqHeaders()["X-Api-Token"]
+	if !ok {
+		return fmt.Errorf("unauthorized")
+	}
+
+	fmt.Println(token[0])
+
+	if err := ParseToken(token[0]); err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func ParseToken(tokenString string) error {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			fmt.Println("Unexpected signing method: %v", token.Header["alg"])
+			return nil, fmt.Errorf("unauthorized")
+		}
+		secret := os.Getenv("JWT_SECRET")
+		return []byte(secret), nil
+	})
+	if err != nil {
+		fmt.Println("parsing error: ", err)
+		return fmt.Errorf("unauthorized")
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		fmt.Println(claims)
+	}
+	return fmt.Errorf("unauthorized")
 }
