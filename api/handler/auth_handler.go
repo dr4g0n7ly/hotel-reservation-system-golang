c82485c65e20ed1a81c641fc47cc8376ef5d455d
dr4g0n7ly <nishanth.pilli@gmail.com>
2024-04-23 01:23:27 +0530

    add getUserByEmail

--- api/handler/auth_handler.go
+++ api/handler/auth_handler.go
@@ -1,10 +1,12 @@
 package api
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/dr4g0n7ly/hotel-management-system-golang/db"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type AuthHandler struct {
@@ -23,12 +25,20 @@ type AuthParams struct {
 }
 
 func (h *AuthHandler) HandleAuthenticate(c *fiber.Ctx) error {
-	var AuthParams AuthParams
-	if err := c.BodyParser(&AuthParams); err != nil {
+	var params AuthParams
+	if err := c.BodyParser(&params); err != nil {
 		return err
 	}
 
-	fmt.Println(AuthParams)
+	user, err := h.userStore.GetUserByEmail(c.Context(), params.Email)
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return fmt.Errorf("invalid credentials")
+		}
+		return err
+	}
+
+	fmt.Println(user)
 
 	return nil
 }
