d64edb2c3bd3ed293137181dbedf340b75395d98
dr4g0n7ly <nishanth.pilli@gmail.com>
2024-04-24 23:49:46 +0530

    Update booking.go

--- types/booking.go
+++ types/booking.go
@@ -7,9 +7,10 @@ import (
 )
 
 type Booking struct {
-	ID       primitive.ObjectID `json:"id" bson:"_id,omitempty"`
-	UserID   primitive.ObjectID `json:"userId" bson:"userId"`
-	RoomID   primitive.ObjectID `json:"roomId" bson:"roomId"`
-	FromDate time.Time          `json:"fromDate" bson:"fromDate"`
-	TillDate time.Time          `json:"tillDate" bson:"tillDate"`
+	ID         primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	UserID     primitive.ObjectID `json:"userId" bson:"userId"`
+	RoomID     primitive.ObjectID `json:"roomId" bson:"roomId"`
+	FromDate   time.Time          `json:"fromDate" bson:"fromDate"`
+	TillDate   time.Time          `json:"tillDate" bson:"tillDate"`
+	NumPersons int                `json:"numPersons" bson:"numPersons"`
 }
