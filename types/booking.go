62e7442d41d318c4ca71a3724d9d3cc98ded07b6
dr4g0n7ly <nishanth.pilli@gmail.com>
2024-04-24 23:37:41 +0530

    Create booking.go

---
+++ types/booking.go
@@ -0,0 +1,15 @@
+package types
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Booking struct {
+	ID       primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	UserID   primitive.ObjectID `json:"userId" bson:"userId"`
+	RoomID   primitive.ObjectID `json:"roomId" bson:"roomId"`
+	FromDate time.Time          `json:"fromDate" bson:"fromDate"`
+	TillDate time.Time          `json:"tillDate" bson:"tillDate"`
+}
