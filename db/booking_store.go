d9e0397ffadf3f4ecf5a5c791dc0e157a82b25ef
dr4g0n7ly <nishanth.pilli@gmail.com>
2024-04-25 18:14:29 +0530

    Create booking_store.go

---
+++ db/booking_store.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"context"
+
+	"github.com/dr4g0n7ly/hotel-management-system-golang/types"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type BookingStore interface {
+	InsertHotel(context.Context, *types.Booking) (*types.Booking, error)
+}
+
+type MongoBookingStore struct {
+	client *mongo.Client
+	coll   *mongo.Collection
+}
+
+func NewMongoBookingStore(client *mongo.Client) *MongoBookingStore {
+	return &MongoBookingStore{
+		client: client,
+		coll:   client.Database(DBNAME).Collection("bookings"),
+	}
+}
+
+func (s *MongoBookingStore) InsertBooking(ctx context.Context, booking *types.Booking) (*types.Booking, error) {
+	res, err := s.coll.InsertOne(ctx, booking)
+	if err != nil {
+		return nil, err
+	}
+	booking.ID = res.InsertedID.(primitive.ObjectID)
+	return booking, nil
+}
