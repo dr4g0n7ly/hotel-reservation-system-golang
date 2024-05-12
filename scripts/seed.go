45d7b7056c6d3e60a6d486518380740b824cc2ae
dr4g0n7ly <nishanth.pilli@gmail.com>
2024-05-12 16:12:00 +0530

    Update seed.go

--- scripts/seed.go
+++ scripts/seed.go
@@ -10,73 +10,9 @@ import (
 	"github.com/dr4g0n7ly/hotel-management-system-golang/db"
 	"github.com/dr4g0n7ly/hotel-management-system-golang/db/fixtures"
 	"github.com/dr4g0n7ly/hotel-management-system-golang/types"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
-
-func seedUser(client *mongo.Client, isAdmin bool, email, firstname, lastname, password string) {
-	userStore := db.NewMongoUserStore(client)
-	user, err := types.NewUserFromParams(types.CreateUserParams{
-		Email:     email,
-		FirstName: firstname,
-		LastName:  lastname,
-		Password:  password,
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	user.IsAdmin = isAdmin
-	insertedUser, err := userStore.InsertUser(context.Background(), user)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(insertedUser)
-}
-
-func seedHotel(client *mongo.Client, name string, location string, rating int) {
-	hotelStore := db.NewMongoHotelStore(client)
-	roomStore := db.NewMongoRoomStore(client, hotelStore)
-
-	hotel := types.Hotel{
-		Name:     name,
-		Location: location,
-		Rooms:    []primitive.ObjectID{},
-		Rating:   rating,
-	}
-	insertedHotel, err := hotelStore.InsertHotel(context.Background(), &hotel)
-	if err != nil {
-		log.Fatal(err)
-	}
-	rooms := []types.Room{
-		{
-			Type:  types.Single,
-			Price: 150.50,
-		},
-		{
-			Type:  types.Single,
-			Price: 150.50,
-		},
-		{
-			Type:  types.Deluxe,
-			Price: 200,
-		},
-		{
-			Type:  types.Suite,
-			Price: 1399,
-		},
-	}
-	for _, room := range rooms {
-		room.HotelID = insertedHotel.ID
-		insertedRoom, err := roomStore.InsertRoom(context.Background(), &room)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(insertedRoom)
-	}
-	fmt.Println(insertedHotel)
-
-}
 
 func main() {
 
